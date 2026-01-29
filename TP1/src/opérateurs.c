125d7a4d52b21b03d6577664f37faa07f8a972c7
Pasidi <167764476+Pasidi-hash@users.noreply.github.com>
2026-01-29 10:41:54 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h> // Pour utiliser le type booléen
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    printf("Valeurs : a = %d, b = %d\n\n", a, b);
+
+    // 1 à 4. Opérations arithmétiques de base
+    printf("Addition (a + b) : %d\n", a + b);
+    printf("Soustraction (a - b) : %d\n", a - b);
+    printf("Multiplication (a * b) : %d\n", a * b);
+    printf("Division entière (a / b) : %d\n", a / b);
+
+    // 5. Modulo (le reste de la division)
+    printf("Modulo (a %% b) : %d\n\n", a % b);
+
+    // 6 et 7. Comparaisons (Logique)
+    // En C, 0 signifie "Faux" et 1 signifie "Vrai"
+    printf("Est-ce que a est égal à b ? : %d\n", a == b);
+    printf("Est-ce que a est supérieur à b ? : %d\n", a > b);
+
+    return 0;
+}
 
 
